f29e504df95cf66ca63a48079ad24e9c9a62f516
kiendeptrai0909-spec <kiendeptrai0909@gmail.com>
2026-05-28 23:48:28 +0700

    add role staff

--- backend/src/main/java/com/example/techstore/config/SecurityConfig.java
+++ backend/src/main/java/com/example/techstore/config/SecurityConfig.java
@@ -89,7 +89,7 @@ public class SecurityConfig {
                                 "/v3/api-docs/**"
                         ).permitAll()
 
-                        .requestMatchers("/api/v1/admin/**").hasAuthority("ROLE_ADMIN")
+                        .requestMatchers("/api/v1/admin/**").hasAnyAuthority("ROLE_ADMIN", "ROLE_STAFF")
                         .requestMatchers("/api/v1/password-reset/**").permitAll()
                         .anyRequest().authenticated()
                 )
